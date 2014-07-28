54b0b50ea5b7f58a7871e999282cc082ccc2415d
Dan Murphy <murphydbuffalo@gmail.com>
2014-07-27 22:15:43 -0400

    Added a non-recursive solution in Ruby.

--- lib/factorial.rb
+++ lib/factorial.rb
@@ -5,3 +5,17 @@ def factorial(number)
     puts 'Please enter an integer 0 or greater :^) .'
   end
 end
+
+def non_recursive_factorial(number)
+  if number >= 0
+    factors = []
+    value = 1
+    number.times do 
+      factors.push(value)
+      value += 1
+    end
+    number <= 1 ? 1 : factors.reduce(1) { |product, num| product * num }
+  else
+    puts 'Please enter an integer 0 or greater :^) .'
+  end
+end
